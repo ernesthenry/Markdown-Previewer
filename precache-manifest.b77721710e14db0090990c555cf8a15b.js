cc550fe75217bacb4ca8ef8c360ac718a01dca24
ernesthenry <henry38ernest@gmail.com>
2022-10-04 16:41:13 -0700

    Updates

---
+++ precache-manifest.b77721710e14db0090990c555cf8a15b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "176d1408eabfa8874ea0",
+    "url": "/markdown-previewer/static/css/main.1c991710.chunk.css"
+  },
+  {
+    "revision": "176d1408eabfa8874ea0",
+    "url": "/markdown-previewer/static/js/main.176d1408.chunk.js"
+  },
+  {
+    "revision": "ebb01f1582699742545c",
+    "url": "/markdown-previewer/static/js/1.ebb01f15.chunk.js"
+  },
+  {
+    "revision": "b2c275135a06121482d1",
+    "url": "/markdown-previewer/static/js/runtime~main.b2c27513.js"
+  },
+  {
+    "revision": "09837471da742c92ad9eecc9a07547ea",
+    "url": "/markdown-previewer/index.html"
+  }
+];
